afca4ef018f9fe765aff15304123018cb70a0076
Sumit Kumar Singh <63084502+Sumit-968@users.noreply.github.com>
2020-04-17 22:41:51 +0530

    Add files via upload

---
+++ ques.a.c
@@ -0,0 +1,76 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+ 
+int mutex=1,full=0,empty=3,x=0;
+ 
+int main()
+{
+	int n;
+	void producer();
+	void consumer();
+	int wait(int);
+	int signal(int);
+	printf("\n1.Producer\n2.Consumer\n3.Exit");
+	while(1)
+	{
+		printf("\nEnter your choice:");
+		scanf("%d",&n);
+		switch(n)
+		{
+			case 1:	if((mutex==1)&&(empty!=0))
+						producer();
+					else
+						printf("Buffer is full!!");
+					break;
+			case 2:	if((mutex==1)&&(full!=0))
+						consumer();
+					else
+						printf("Buffer is empty!!");
+					break;
+			case 3:
+					exit(0);
+					break;
+		}
+	}
+	
+	return 0;
+}
+ 
+int wait(int s)
+{
+	return (--s);
+}
+ 
+int signal(int s)
+{
+	return(++s);
+}
+ 
+void producer()
+{
+	mutex=wait(mutex);
+	full=signal(full);
+	empty=wait(empty);
+	x++;
+	printf("\nProducer produces the item %d",x);
+	mutex=signal(mutex);
+}
+ 
+void consumer()
+{
+	mutex=wait(mutex);
+	full=wait(full);
+	empty=signal(empty);
+	printf("\nConsumer consumes item %d",x);
+	x--;
+	mutex=signal(mutex);
+}
